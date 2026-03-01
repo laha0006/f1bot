a6ffd06457e5f1880125a40d751f02024ae0bda6
Lars <144362461+laha0006@users.noreply.github.com>
2026-03-01 20:43:50 +0100

    reworked ferver dream, to use sliding widnow log

--- bucket.js
+++ bucket.js
@@ -1,72 +1,17 @@
-const second = 1000;
-const minute = 60000;
-
-let limit = 60;
-let started = undefined;
-let startedBurst = undefined;
-let burstCount = 5;
-
-let requests = 0;
-
-function refreshBurst() {
-    burstCount = 5;
-}
-function refreshLimit() {
-    limit = 60;
-}
+let secondLog = [];
+let minuteLog = [];
 
 export function gate() {
     const now = performance.now();
 
-    let timeSinceStartedBurst;
-    let timeSinceStarted;
+    secondLog = secondLog.filter((ts) => now - ts <= 1000);
+    minuteLog = minuteLog.filter((ts) => now - ts <= 60000);
 
-    if (started !== undefined) {
-        timeSinceStartedBurst = now - startedBurst;
-        timeSinceStarted = now - started;
-    }
-
-    if (timeSinceStartedBurst < second) {
-        if (burstCount > 0) burstCount--;
-    } else {
-        startedBurst = now;
-        refreshBurst();
-    }
-
-    if (timeSinceStarted < minute) {
-        if (limit > 0) limit--;
-    } else {
-        started = now;
-        refreshLimit();
-    }
-
-    console.log(`limit: ${limit} , burst: ${burstCount}`);
-
-    if (limit === 0) {
-        console.log("LIMIT BREAK");
-        console.log(timeSinceStarted);
-        console.log("reqeusts", requests);
+    if (secondLog.length >= 5 || minuteLog.length >= 60) {
         return false;
     }
+    secondLog.push(now);
+    minuteLog.push(now);
 
-    if (burstCount === 0) {
-        console.log("BURSTED");
-        console.log(timeSinceStartedBurst);
-
-        console.log("limit", limit);
-        console.log("burst", burstCount);
-        console.log("now          ", now);
-        console.log("startedBurst ", startedBurst);
-        console.log("reqeusts", requests);
-
-        return false;
-    }
-    requests++;
     return true;
-}
-
-export function sanity() {
-    const now = Date.now();
-    console.log(now);
-    return now;
 }
