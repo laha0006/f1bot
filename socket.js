fcf1aaec81b7b0e37c9c561ffd0299eabed1e27c
Lars <144362461+laha0006@users.noreply.github.com>
2026-03-07 15:00:17 +0100

    example socket usage

---
+++ socket.js
@@ -0,0 +1,54 @@
+import mqtt from "mqtt";
+import { getToken } from "./login.js";
+
+const accessToken = await getToken();
+
+const websocketUrl = "wss://mqtt.openf1.org:8084/mqtt";
+
+const options = {
+    username: "tolana",
+    password: accessToken,
+};
+
+const client = mqtt.connect(websocketUrl, options);
+
+client.on("connect", function () {
+    console.log("Connected to OpenF1 via Websockets");
+    client.subscribe("v1/location", function (err) {
+        if (!err) {
+            console.log("Subscribed to v1/location");
+        } else {
+            console.error("Subscription error for v1/location:", err);
+        }
+    });
+    client.subscribe("v1/laps", function (err) {
+        if (!err) {
+            console.log("Subscribed to v1/laps");
+        } else {
+            console.error("Subscription error for v1/laps:", err);
+        }
+    });
+    // client.subscribe('#'); // Subscribe to all topics
+});
+
+client.on("message", function (topic, message) {
+    console.log(`Received on ${topic}: ${message.toString()}`);
+    // const data = JSON.parse(message.toString());
+    // Process data
+});
+
+client.on("error", function (error) {
+    console.error("MQTT Connection Error:", error);
+});
+
+client.on("close", function () {
+    console.log("MQTT Connection closed");
+});
+
+client.on("offline", function () {
+    console.log("MQTT Client is offline");
+});
+
+client.on("reconnect", function () {
+    console.log("MQTT Client is attempting to reconnect");
+});
