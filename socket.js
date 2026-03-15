63e8b7ade3df298b86a45580adcf0c009e33613b
Lars <144362461+laha0006@users.noreply.github.com>
2026-03-15 09:19:17 +0100

    changed output, and saved data ot .txt files

--- socket.js
+++ socket.js
@@ -2,6 +2,7 @@ import mqtt from "mqtt";
 import { getToken } from "./login.js";
 
 const accessToken = await getToken();
+console.log("token: ", accessToken);
 
 const websocketUrl = "wss://mqtt.openf1.org:8084/mqtt";
 
@@ -14,34 +15,33 @@ const client = mqtt.connect(websocketUrl, options);
 
 client.on("connect", function () {
     console.log("Connected to OpenF1 via Websockets");
-    client.subscribe("v1/location", function (err) {
+    // client.subscribe("v1/location", function (err) {
+    //     if (!err) {
+    //         console.log("Subscribed to v1/location");
+    //     } else {
+    //         console.error("Subscription error for v1/location:", err);
+    //     }
+    // });
+    // client.subscribe("v1/laps", function (err) {
+    //     if (!err) {
+    //         console.log("Subscribed to v1/laps");
+    //     } else {
+    //         console.error("Subscription error for v1/laps:", err);
+    //     }
+    // });
+    client.subscribe("v1/race_control", function (err) {
         if (!err) {
-            console.log("Subscribed to v1/location");
+            console.log("Subscribed to v1/race_control");
         } else {
-            console.error("Subscription error for v1/location:", err);
+            console.error("Subscription error for v1/race_control:", err);
         }
     });
-    client.subscribe("v1/laps", function (err) {
-        if (!err) {
-            console.log("Subscribed to v1/laps");
-        } else {
-            console.error("Subscription error for v1/laps:", err);
-        }
-    });
-    client.subscribe("v1/racecontrol", function (err) {
-        if (!err) {
-            console.log("Subscribed to v1/racecontrol");
-        } else {
-            console.error("Subscription error for v1/racecontrol:", err);
-        }
-    });
-    client.subscribe("#"); // Subscribe to all topics
+    //client.subscribe("v1/race_control"); // Subscribe to all topics
 });
-
 client.on("message", function (topic, message) {
-    console.log(`Received on ${topic}: ${message.toString()}`);
-    // const data = JSON.parse(message.toString());
-    // Process data
+    //console.log(`Received on ${topic}: ${message.toString()}`);
+    const data = JSON.parse(message.toString());
+    console.log(topic, data.message);
 });
 
 client.on("error", function (error) {
